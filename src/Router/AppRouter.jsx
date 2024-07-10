56b0c5880f292f0c2513efbbde4671076fdec9b1
Sonal Sai K S <sonalsai777@gmail.com>
2024-07-10 10:32:09 -0700

    initial commit

---
+++ src/Router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Index from "../pages/Index";
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Index/>} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRouter
