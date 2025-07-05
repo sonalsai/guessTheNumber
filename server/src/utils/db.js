738ba9cf3bce4ed5937e754f8a8a81492247c8de
Sonal Sai K S <64025222+sonalsai@users.noreply.github.com>
2025-07-06 00:26:14 +0530

    refactor: improve MongoDB connection handling and ensure single instance usage

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -3,11 +3,19 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.MONGO_URI;
 
 const client = new MongoClient(uri);
+let _db; // Variable to store the connected database instance
+
 async function connectDB() {
+  if (_db) {
+    console.log("Already connected to MongoDB, returning existing connection.");
+    return _db;
+  }
+
   try {
     await client.connect();
     console.log("Connected to MongoDB");
-    return client.db("guessTheNumber");
+    _db = client.db("guessTheNumber");
+    return _db;
   } catch (err) {
     console.error("Failed to connect to MongoDB", err);
     process.exit(1);
